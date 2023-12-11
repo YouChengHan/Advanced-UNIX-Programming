d54b602bb6daf5947e2dd73a287249c30c9b89aa
CH <kevin90728@gmail.com>
2023-12-12 00:09:37 +0800

    Fix pathname

--- assignment11/assignment11.c
+++ assignment11/assignment11.c
@@ -11,7 +11,7 @@ int main() {
 	daemonize(cmd);
 
 	// need to use absolute path since the working directory has been set to root. (detached)
-	FILE *fp=fopen("/home/kevin/UNIX/assignment11/assignment11.txt", "w");
+	FILE *fp=fopen("/root/UNIX/assignment11/assignment11.txt", "w");
 	char *name=getlogin();
 	fprintf(fp, "Login name: %s\n", name);
 	//syslog(LOG_NOTICE, "Login name: %s\n", name);
