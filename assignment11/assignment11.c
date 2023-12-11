1ee8236e7ba6755641701a946c150b9d978d64d3
CH <kevin90728@gmail.com>
2023-12-12 00:05:15 +0800

    Finish assignment 11

---
+++ assignment11/assignment11.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <syslog.h>
+
+extern void daemonize(const char *cmd);
+
+int main() {
+
+	// daemonize
+	char *cmd="assignment11d";
+	daemonize(cmd);
+
+	// need to use abolute path since the working directory has been set to root. (detached)
+	FILE *fp=fopen("/home/kevin/UNIX/assignment11/assignment11.txt", "w");
+	char *name=getlogin();
+	fprintf(fp, "Login name: %s\n", name);
+	//syslog(LOG_NOTICE, "Login name: %s\n", name);
+	//closelog();
+
+	fclose(fp);
+	return 0;
+}
